46b0ee13b4f42e7c1ec9cc74c36093dd1ba89a06
talhakashifa <talhakashif067@gmail.com>
2024-07-14 15:03:58 +0500

    todo_list project complete

---
+++ index.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let todos: string[]=[];
+let loop=true;
+while(loop){
+    const answers:{
+         todo: string,
+         addmore:boolean,
+    }
+    = await inquirer.prompt([
+        {
+           type: "input",
+           name:"todo",
+           message:"what do you want to add in your todolist?"
+    },
+    {
+        type:"confirm",
+        name:"addmore",
+        message:"Do you want to add more todo?",
+        default:false
+    }
+])
+const {todo,addmore}=answers;
+console.log(answers)
+loop=addmore
+if(todo){
+todos.push(todo)
+}else{
+    console.log("kindly add valid input.")
+}
+}
+if(todos.length>0){
+    console.log("your todo list: \n")
+ todos.forEach(todo =>{
+    console.log(todo)
+ })
+}else{
+    console.log("No todos found")
+}
